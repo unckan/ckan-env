0534ea38046c5d3edda560c664c94b7dff688be2
Andres Vazquez <andres@data99.com.ar>
2020-06-05 00:05:53 -0300

    Deploy all branches as :develop and master as :latest

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,10 @@ VERSION=$TRAVIS_BRANCH
 
 echo "Docker login"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-echo "Docker push 1"
-docker push ${IMAGE}:${VERSION}
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Docker push to :latest"
     docker push ${IMAGE}:latest
 else
-    echo "Docker NO push to :latest"
+    docker push ${IMAGE}:develop
 fi
