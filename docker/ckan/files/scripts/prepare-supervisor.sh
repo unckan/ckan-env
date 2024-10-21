3f076ac8096314d9f938ac3cedf1cb02a6e22751
avdata99 <andres@data99.com.ar>
2024-10-21 20:15:39 -0300

    fix envsubst var

--- docker/ckan/files/scripts/prepare-supervisor.sh
+++ docker/ckan/files/scripts/prepare-supervisor.sh
@@ -14,9 +14,9 @@ cat /etc/supervisor/conf.d/ckan-worker.conf
 
 # Prepaer the ckan supervisor command
 if [ "$IS_DEV_ENV" = "true" ] ; then
-    CMD="$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --chdir $APP_DIR --certfile=$APP_DIR/files/cert/localhost.cert --keyfile=$APP_DIR/files/cert/localhost.key --reload wsgi:application"
+    export CMD="$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --chdir $APP_DIR --certfile=$APP_DIR/files/cert/localhost.cert --keyfile=$APP_DIR/files/cert/localhost.key --reload wsgi:application"
 else
-    CMD="$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --chdir $APP_DIR wsgi:application --timeout 360"
+    export CMD="$APP_DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --chdir $APP_DIR wsgi:application --timeout 360"
 fi
 
 envsubst < $APP_DIR/files/etc/ckan.conf > /etc/supervisor/conf.d/ckan.conf
