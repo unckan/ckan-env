6af5e8891960e39cf22a22580aeb6a2702a5ba27
avdata99 <andres@data99.com.ar>
2024-10-23 20:33:33 -0300

    Mostrar la version en el footer

--- docker/ckan/files/scripts/entrypoint.sh
+++ docker/ckan/files/scripts/entrypoint.sh
@@ -38,6 +38,8 @@ DATAPUSHER_TOKEN=$(ckan user token add default datapusher_multi expires_in=365 u
 ckan config-tool ckan.ini "ckan.datapusher.api_token=${DATAPUSHER_TOKEN}"
 ckan config-tool ckan.ini "ckanext.datapusher_plus.api_token=${DATAPUSHER_TOKEN}"
 
+ckan config-tool ckan.ini "ckanext.unckan.version=${CKAN_UNI_VERSION}"
+
 # Rebuild webassets in can they were patched
 ckan asset build
 
