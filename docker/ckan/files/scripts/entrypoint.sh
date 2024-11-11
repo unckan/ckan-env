7a45165e5b5188981458e0d6d580189577450ae0
avdata99 <andres@data99.com.ar>
2024-11-11 17:03:06 -0300

    Fixes for datapusher+

--- docker/ckan/files/scripts/entrypoint.sh
+++ docker/ckan/files/scripts/entrypoint.sh
@@ -32,12 +32,6 @@ ckan db upgrade -p datapusher_plus
 echo "Applying migrations for tracking"
 ckan db upgrade -p api_tracking
 
-# Datapusher+ requires a valid API token to operate
-echo "Creating a valid API token for Datapusher+"
-DATAPUSHER_TOKEN=$(ckan user token add default datapusher_multi expires_in=365 unit=86400 | tail -n 1 | tr -d '\t')
-ckan config-tool ckan.ini "ckan.datapusher.api_token=${DATAPUSHER_TOKEN}"
-ckan config-tool ckan.ini "ckanext.datapusher_plus.api_token=${DATAPUSHER_TOKEN}"
-
 # Rebuild search index
 ckan search-index rebuild
 
