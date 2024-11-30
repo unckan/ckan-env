bf2617afaac0d01c057403a33aa30ba15c894538
avdata99 <andres@data99.com.ar>
2024-11-30 09:36:11 -0300

    Move to pusher 0.1.3

--- docker/ckan/files/scripts/install-extensions.sh
+++ docker/ckan/files/scripts/install-extensions.sh
@@ -46,7 +46,7 @@ pip install -e git+https://github.com/okfn/ckanext-announcements.git@0.1.3#egg=c
 pip install -r https://raw.githubusercontent.com/okfn/ckanext-announcements/0.1.3/requirements.txt
 
 echo "Installing Push Errors extension"
-pip install -e git+https://github.com/unckan/ckanext-push-errors.git@0.1.2#egg=ckanext-push-errors
-pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.2/requirements.txt
+pip install -e git+https://github.com/unckan/ckanext-push-errors.git@0.1.3#egg=ckanext-push-errors
+pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.3/requirements.txt
 
 echo "CKAN extensions installed"
