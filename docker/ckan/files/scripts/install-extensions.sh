5f06b6c53139d3d0558e2a876509edf07eb6ebc8
german <germankay@gmail.com>
2025-01-13 15:22:21 -0300

    update install-extensions.sh

--- docker/ckan/files/scripts/install-extensions.sh
+++ docker/ckan/files/scripts/install-extensions.sh
@@ -34,16 +34,16 @@ pip install -e git+https://github.com/okfn/datapusher-plus.git@okfn_tmp#egg=data
 pip install -r https://raw.githubusercontent.com/okfn/datapusher-plus/okfn_tmp/requirements.txt
 
 echo "Installing API-tracking extension"
-pip install git+https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git@0.4.4#egg=ckanext-api-tracking
-pip install -r https://raw.githubusercontent.com/NorwegianRefugeeCouncil/ckanext-api-tracking/refs/tags/0.4.4/requirements.txt
+pip install -e git+https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git@0.4.5#egg=ckanext-api-tracking
+pip install -r https://raw.githubusercontent.com/NorwegianRefugeeCouncil/ckanext-api-tracking/refs/tags/0.4.5/requirements.txt
 
 echo "Installing Apache Superset extension"
 pip install git+https://github.com/unckan/ckanext-superset.git@0.1.9#egg=ckanext-superset
 pip install -r https://raw.githubusercontent.com/unckan/ckanext-superset/refs/tags/0.1.9/requirements.txt
 
 echo "Installing Announcements extension"
-pip install git+https://github.com/okfn/ckanext-announcements.git@0.1.3#egg=ckanext-announcements
-pip install -r https://raw.githubusercontent.com/okfn/ckanext-announcements/0.1.3/requirements.txt
+pip install git+https://github.com/okfn/ckanext-announcements.git@0.1.4#egg=ckanext-announcements
+pip install -r https://raw.githubusercontent.com/okfn/ckanext-announcements/0.1.4/requirements.txt
 
 echo "Installing Push Errors extension"
 pip install git+https://github.com/unckan/ckanext-push-errors.git@0.1.4#egg=ckanext-push-errors
