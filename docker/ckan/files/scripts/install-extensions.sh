891bcf33c8df69e3a0c75302f93816a3e390194f
avdata99 <andres@data99.com.ar>
2025-03-10 09:12:28 -0300

    Release 0.5.0

--- docker/ckan/files/scripts/install-extensions.sh
+++ docker/ckan/files/scripts/install-extensions.sh
@@ -34,8 +34,8 @@ pip install -e git+https://github.com/okfn/datapusher-plus.git@okfn_tmp#egg=data
 pip install -r https://raw.githubusercontent.com/okfn/datapusher-plus/okfn_tmp/requirements.txt
 
 echo "Installing API-tracking extension"
-pip install -e git+https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git@0.4.5#egg=ckanext-api-tracking
-pip install -r https://raw.githubusercontent.com/NorwegianRefugeeCouncil/ckanext-api-tracking/refs/tags/0.4.5/requirements.txt
+pip install -e git+https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git@0.5.1#egg=ckanext-api-tracking
+pip install -r https://raw.githubusercontent.com/NorwegianRefugeeCouncil/ckanext-api-tracking/refs/tags/0.5.1/requirements.txt
 
 echo "Installing Apache Superset extension"
 pip install git+https://github.com/unckan/ckanext-superset.git@0.1.9#egg=ckanext-superset
@@ -46,11 +46,11 @@ pip install git+https://github.com/okfn/ckanext-announcements.git@0.1.4#egg=ckan
 pip install -r https://raw.githubusercontent.com/okfn/ckanext-announcements/0.1.4/requirements.txt
 
 echo "Installing Push Errors extension"
-pip install git+https://github.com/unckan/ckanext-push-errors.git@0.1.4#egg=ckanext-push-errors
-pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.4/requirements.txt
+pip install git+https://github.com/unckan/ckanext-push-errors.git@0.1.5#egg=ckanext-push-errors
+pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.5/requirements.txt
 
 echo "Installing DBQuery extension"
-pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.1.0#egg=ckanext-dbquery
-pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.1.0/requirements.txt
+pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.2.0#egg=ckanext-dbquery
+pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.2.0/requirements.txt
 
 echo "CKAN extensions installed"
