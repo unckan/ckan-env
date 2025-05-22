4d15033e88924da080e216563090dc345bf0c8fd
avdata99 <andres@data99.com.ar>
2025-05-21 21:17:06 -0300

    Update dbquery

--- docker/ckan/files/scripts/install-extensions.sh
+++ docker/ckan/files/scripts/install-extensions.sh
@@ -50,7 +50,7 @@ pip install git+https://github.com/unckan/ckanext-push-errors.git@0.1.5#egg=ckan
 pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.5/requirements.txt
 
 echo "Installing DBQuery extension"
-pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.2.0#egg=ckanext-dbquery
-pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.2.0/requirements.txt
+pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.2.1#egg=ckanext-dbquery
+pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.2.1/requirements.txt
 
 echo "CKAN extensions installed"
--- docker/ckan/files/scripts/install-python.sh
+++ docker/ckan/files/scripts/install-python.sh
@@ -3,6 +3,7 @@
 echo "Installing Python"
 
 # =====================================================
+apt update
 # Install Python3 with the default version
 apt install -y python3 python3-dev python3-pip python3-venv
 echo "Python3 installed $(which python3)"
