b52bdb6a5046229229af4d3f26d9f11358dd263f
avdata99 <andres@data99.com.ar>
2025-06-09 14:02:39 -0300

    mihgrate on clean

--- ckanext-unckan/ckanext/unckan/tests/conftest.py
+++ ckanext-unckan/ckanext/unckan/tests/conftest.py
@@ -2,5 +2,8 @@ import pytest
 
 
 @pytest.fixture
-def clean_db(reset_db):
+def clean_db(reset_db, migrate_db_for):
+    migrate_db_for("activity")
+    migrate_db_for("announcements")
+    migrate_db_for("api_tracking")
     reset_db()
