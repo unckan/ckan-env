8974b0501d1a039cc4dc5ddf3244068c1662ac12
avdata99 <andres@data99.com.ar>
2025-06-09 14:16:54 -0300

    Fix conftest

--- ckanext-unckan/ckanext/unckan/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def clean_db(reset_db, migrate_db_for):
-    migrate_db_for("activity")
-    migrate_db_for("announcements")
-    migrate_db_for("api_tracking")
-    reset_db()
--- ckanext-unckan/ckanext/unckan/tests/fixtures.py
+++ ckanext-unckan/ckanext/unckan/tests/fixtures.py
@@ -5,3 +5,11 @@ import pytest
 @pytest.fixture(autouse=True)
 def load_standard_plugins(with_plugins):
     pass
+
+
+@pytest.fixture
+def clean_db(reset_db, migrate_db_for):
+    migrate_db_for("activity")
+    migrate_db_for("announcements")
+    migrate_db_for("api_tracking")
+    reset_db()
