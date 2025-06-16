b89f98baa1d8576cdd7ad9d71bbe223913e3f580
German Favaro <germankay@gmail.com>
2025-06-16 11:42:05 -0300

    dbquery 0.2.3

--- docker/ckan/files/scripts/install-extensions.sh
+++ docker/ckan/files/scripts/install-extensions.sh
@@ -50,8 +50,8 @@ pip install git+https://github.com/unckan/ckanext-push-errors.git@0.1.6#egg=ckan
 pip install -r https://raw.githubusercontent.com/unckan/ckanext-push-errors/refs/tags/0.1.6/requirements.txt
 
 echo "Installing DBQuery extension"
-pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.2.2#egg=ckanext-dbquery
-pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.2.2/requirements.txt
+pip install -e git+https://github.com/unckan/ckanext-dbquery.git@0.2.3#egg=ckanext-dbquery
+pip install -r https://raw.githubusercontent.com/unckan/ckanext-dbquery/refs/tags/0.2.3/requirements.txt
 
 echo "Installing Ckanext Selfinfo extension"
 pip install -e git+https://github.com/DataShades/ckanext-selfinfo.git@v1.2.0#egg=ckanext-selfinfo
